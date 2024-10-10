4f036c1f1674e661ac7a6c169425b40c95e04d73
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-10-10 10:07:53 +0300

    added save_chosen_to_file method

--- src/file_tool.py
+++ src/file_tool.py
@@ -25,11 +25,21 @@ class JsonFileTool(BaseFileTool):
         with open(self.path, "w", encoding="utf-8") as json_file:
             json.dump(ready_to_save, json_file, indent=4, ensure_ascii=False)
 
-    def remove_from_file(self, numbers: list) -> None:
+
+    def save_chosen_to_file(self, vacancy_list: list, index_numbers: list) -> None:
+        """Method rewriting or creating a new file with given vacancies"""
+        ready_to_save = []
+        for number in sorted(index_numbers, reverse=True):
+            ready_to_save.append(vacancy_list[number].vacancy_info)
+        with open(self.path, "w", encoding="utf-8") as json_file:
+            json.dump(ready_to_save, json_file, indent=4, ensure_ascii=False)
+
+
+    def remove_from_file(self, index_numbers: list) -> None:
         """method removing a vacancy by a given list of index numbers"""
         with open(self.path, "r", encoding="utf-8") as json_file:
             vacancies = json.load(json_file)
-            for number in sorted(numbers, reverse=True):
+            for number in sorted(index_numbers, reverse=True):
                 vacancies.pop(number)
         with open(self.path, "w", encoding="utf-8") as json_file:
             json.dump(vacancies, json_file, indent=4, ensure_ascii=False)
